acbf0e2eb2d8dd69c85479f33436de77bfb7e4ae
AlexVotry <alexvotry@gmail.com>
2016-11-14 09:20:30 -0800

    [wip] modified images folder

--- public/picture-book/component.js
+++ public/picture-book/component.js
@@ -19,8 +19,8 @@
     var vm = this;
     var flipped = false;
     var slides = vm.slides = [];
-    var picture = ['images/finger.jpg', 'images/leaderJersey.jpg', 'images/Vol2015_3.jpg', 'images/TeamPursuitGold.jpg' ];
-    var caption = ['Nice image','Awesome photograph','That is so cool','I love that'];
+    var picture = ['images/basilica.jpg', 'images/venice.jpg', 'images/jersey.jpg', 'images/prague.jpg', 'images/TeamPursuitGold.jpg', 'images/thai_head.jpg', 'images/elephant.jpg', 'images/parthenon.jpg', 'images/santorini.jpg', 'images/fuji.jpg', 'images/jpn2.jpg', 'images/kimono.jpg', 'images/kid.jpg' ];
+    var caption = ['Thanks for looking!', 'basilica', 'venice', 'Leader Jersey', 'Prague', 'TeamPursuitGold', 'Thailand', 'Riding an elephant in Thailand', 'The Parthenon', 'Lost City of Atlantis', 'The top of Mt. Fuji', 'Dinner with friends', 'When in Japan..' ];
 
     (vm.showSlide = function() {
       for (var i = 0; i < picture.length; i++) {
@@ -30,15 +30,20 @@
 
     vm.advance = function(index) {
       var currentPic = '.pic' + index;
+      var previous = '.pic' + (index + 1);
       var z = picture.length - index;
       console.log('Az: ', z);
-    $('.pic'+ index).parent().addClass('flip').css({ 'z-index': z });
+      $(currentPic).parent().addClass('flip').css({ 'z-index': z });
+      // if (previous) {
+      //   $('.flip').css({ 'z-index': 0 });
+      // }
     }
 
     vm.goBack = function(index) {
       console.log('Bz: ', index);
       var currentPic = '.pic' + (index - 1);
       $('.pic'+ (index - 1)).parent().removeClass('flip').css({ 'z-index': index });
+      // $('.pic'+ (index)).parent().css({ 'z-index': 0  });
     }
   };
 
